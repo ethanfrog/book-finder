dbc4412b0e3919f1147d7b5f835c04f2b2d668d6
ethanfrog <101209254+ethanwynne395@users.noreply.github.com>
2024-03-18 23:54:46 -0400

    Added loginUser and addUser mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,21 +3,42 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        me: async () => {
+        me: async (parent) => {
 
         }
     },
     Mutation: {
-        loginUser: async () => {
+        loginUser: async (parent, { email, password }) => {
+            // Find profile based on email
+            const profile = await Profile.findOne({ email });
+
+            if (!profile) {
+                throw AuthenticationError;
+            }
+
+            // Fail to log in if incorrect password is given
+            const correctPw = await profile.isCorrectPassword(password);
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+
+            const token = signToken(profile);
+            return { token, profile };
+        },
+
+        addUser: async (parent, args) => {
+            //Create new user with given arguments
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { user, token };
+        },
+
+        saveBook: async (parent) => {
             
         },
-        addUser: async () => {
-            
-        },
-        saveBook: async () => {
-            
-        },
-        removeBook: async () => {
+
+        removeBook: async (parent) => {
             
         }
     }
