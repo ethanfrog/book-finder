929447829cb656423ff9ee9197a0c5105cbe9393
ethanfrog <101209254+ethanwynne395@users.noreply.github.com>
2024-03-18 23:34:16 -0400

    Added the required types to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,39 @@
+//! means it cannot be NULL
+
 const typeDefs = `
 
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookData: BookInput!): User
+        removeBook(bookId: ID!): User
+    }
+
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+      }
+
+    type Book {
+        bookId: ID!
+        authors: [String]!
+        description: String
+        title: String!
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
 `;
 
 module.exports = typeDefs;
