5011b21a9bd002c41f81429ed0112ee250f58240
ethanfrog <101209254+ethanwynne395@users.noreply.github.com>
2024-03-18 23:38:03 -0400

    Added the shells of needed queries and mutations in resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,9 +3,22 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
     Query: {
+        me: async () => {
 
+        }
     },
     Mutation: {
-        
+        loginUser: async () => {
+            
+        },
+        addUser: async () => {
+            
+        },
+        saveBook: async () => {
+            
+        },
+        removeBook: async () => {
+            
+        }
     }
 }
